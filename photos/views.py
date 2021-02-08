d7465e45e659a0041b886075076b82c322685125
Spencer <spencer.peq@gmail.com>
2021-02-07 18:34:53 -0800

    Created models Photo and Category, static files, render data to template

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
+from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'photos': photos}
+    return render(request, 'photos/gallery.html', context)
 
 def view_photo(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    context = {'photo': photo}
+    return render(request, 'photos/photo.html', context)
 
 def add_photo(request):
     return render(request, 'photos/add.html')
