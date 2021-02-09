8f8ed9634276f7111b4a199f9d5925210c0d4c29
Spencer <spencer.peq@gmail.com>
2021-02-08 19:39:27 -0800

    Add new image functionality

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 # Create your views here.
 
@@ -14,4 +14,26 @@ def view_photo(request, pk):
     return render(request, 'photos/photo.html', context)
 
 def add_photo(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+        
+        photo = Photo.objects.create(
+            category = category, 
+            description = data['description'],
+            image = image,
+        )
+        return redirect('gallery')
+
+    context = {'categories': categories}
+
+    return render(request, 'photos/add.html', context)
